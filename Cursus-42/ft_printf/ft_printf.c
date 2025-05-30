4de0ada8011fed9bfce2ed3fef633a1db8737c3c
Vilab <143715482+Vilab-unavailable@users.noreply.github.com>
2025-05-30 16:05:33 +0200

    Update ft_printf.c

--- Cursus-42/ft_printf/ft_printf.c
+++ Cursus-42/ft_printf/ft_printf.c
@@ -15,23 +15,40 @@ int	ft_printchar(char c)
 	return (write(1, &c, 1));
 }
 
-int	ft_printstr
-		
+int	ft_printstr(char *)
+{
+	size_t	i;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (s[i])
+	{
+		len += ft_printchar(s[i]);
+		i++;
+	}
+	return (len);
+}
+
 int	ft_type(va_list args, const char c)
 {
 	int	len;
 
 	len = 0;
 	if (c == 'c')
-		
+		len = ft_printchar(va_arg(args, int));
 	else if (c == 's')
+		len = ft_printstr(va_arg(args, char *));
 	else if (c == 'p')
+		
 	else if (c == 'd')
 	else if (c == 'i')
 	else if (c == 'u')
 	else if (c == 'x')
 	else if (c == 'X')
 	else if (c == '%')
+	return (len);
+}
 		
 
 
@@ -43,14 +60,14 @@ int	ft_printf(const char *str, ...)
 
 	len = 0;
 	i = 0;
-	if (!s)
+	if (!str)
 		return (-1);
 	va_start(args, str);
-	while (str[i] & len >= 0)
+	while (str[i])
 	{
 		if (str[i] == '%')
 		{
-			len = ft_type(args, str[i + 1], len);
+			len += ft_type(args, str[i + 1]);
 			i++ ;
 		}
 		else
@@ -60,5 +77,10 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (len);
 }
-		
-			
+int main()
+{
+    ft_putnbr_fd(ft_printf("BBB%s", "copie"), 1);
+    write(1, "\n",1);
+    ft_putnbr_fd(printf("A%s", "vrai"), 1);
+    return (0);
+}		
