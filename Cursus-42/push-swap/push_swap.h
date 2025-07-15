9886e171e41c8c2f86bf3ebea9302a06571d0d67
Vilab <143715482+Vilab-unavailable@users.noreply.github.com>
2025-07-15 17:32:45 +0200

    Add files via upload

--- Cursus-42/push-swap/push_swap.h
+++ Cursus-42/push-swap/push_swap.h
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vilabard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 15:22:22 by vilabard          #+#    #+#             */
+/*   Updated: 2025/07/15 15:22:25 by vilabard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
-
+/*
 typedef struct s_data
 {
 	long	*stack_a;
@@ -8,7 +20,20 @@ typedef struct s_data
 	size_t 	size_stack_a;
 	size_t	size_stack_b;
 }	t_data;
+*/
 
+typedef struct s_stack_a
+{
+	long	value
+	long	index
+	struct s_stack_a *next
+}	t_stack_a;
 
+typedef struct s_stack_b
+{
+	long	value
+	long	index
+	struct s_stack_b *next
+}	t_stack_b;
 
 #endif
