cb06a76fba1f536dbbc333ba9541e03fd76220d0
Vilab <143715482+Vilab-unavailable@users.noreply.github.com>
2025-08-08 07:58:03 +0200

    Create fractol.h

---
+++ Cursus-42/fractol/fractol.h
@@ -0,0 +1,17 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <math.h>
+# include <mlx.h>
+# include "libft/libft.h"
+
+# define W_WIDTH 			800
+# define W_HEIGHT			600
+
+# define ESC 53
+
+# define SCROLL_UP 4
+# define SCROLL_DOWN 5
