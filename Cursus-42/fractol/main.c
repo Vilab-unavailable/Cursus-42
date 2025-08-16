b9d99ea887083abd81f0cfffb178ad545c2671bc
Vilab <143715482+Vilab-unavailable@users.noreply.github.com>
2025-08-16 13:39:39 +0200

    Update main.c

--- Cursus-42/fractol/main.c
+++ Cursus-42/fractol/main.c
@@ -1,30 +1,72 @@
 #include "fractol.h"
-void calc_julia
-
-int draw_fractal(t_fdata *fdata)
+void calc_julia(t_f *f)
 {
- fdata->x = 0;
- while (fdata->x < WIDTH)
+ int    i;
+ double temp_zx;
+
+ f->zx = (f->x / f->zoom) + f->offset_x;
+ f->zy = (f->y / f->zoom) + f->offset_y;
+ i = 0;
+ while ( i < f->max_iter || (f->zx * f->zx) + (f->zy * f->zy) >= DOUBLE_MAX)
+ {
+  temp_zx = f->zx;
+  f->zx = (f->zx * f->zx) - (f->zy * f->zy) + f->cx;
+  f->zy = 2 * f->zy * temp_zx + f->cy;
+  i++ ;
+ }
+ if (i == f->max_iter)
+  color_pixel(f, 0x000000);
+ else
+  color_pixel(f, i); 
+}
+
+void calc_mandelbrot(tf *f)
+{
+  int    i;
+ double temp_zx;
+
+ f->zx = 0.0;
+ f->zy = 0.0;
+ f->cx = (f->x / f->zoom) + f->offset_x;
+ f->cy = (f->y / f->zoom) + f->offset_y;
+ i = 0;
+ while ( i < f->max_iter || (f->zx * f->zx) + (f->zy * f->zy) >= DOUBLE_MAX)
+ {
+  temp_zx = (f->zx * f->zx) - (f->zy * f->zy) + f->cx;
+  f->zx = (f->zx * f->zx) - (f->zy * f->zy) + f->cx;
+  f->zy = 2 * f->zy * temp_zx + f->cy;
+  i++ ;
+ }
+ if (i == f->max_iter)
+  color_pixel(f, 0x000000);
+ else
+  color_pixel(f, i); 
+}
+
+int draw_fractal(t_f *f)
+{
+ f->x = 0;
+ while (f->x < WIDTH)
  {
   fdata->y = 0;
-  while (fdata->y < HEIGHT)
+  while (f->y < HEIGHT)
   {
-   if (fdata->f_type == 'j')
+   if (f->f_type == 'j')
    {
-    if (f_data->custom_c == 0)
+    if (f->custom_c == 0)
     {
-     f_data->cx = 0.285;
-     f_data->cy = 0.01;
+     f->cx = 0.285;
+     f->cy = 0.01;
     }
-    calc_julia(fdata, cx, cy);
+    calc_julia(f, cx, cy);
    }
-   else if (fdata->f_type == 'm')
-    calc_mandelbrot(fdata);
+   else if (f->f_type == 'm')
+    calc_mandelbrot(f);
    fractal->y++;
   }
   fractal->x++;
  }
- mlx_put_image_to_window(fdata->mlx, fdata->mlx_win, fdata->image, 0,
+ mlx_put_image_to_window(f->mlx, f->mlx_win, f->mlx_img, 0,
   0);
  return (0);
 }
@@ -37,13 +79,13 @@ int  key_hook(int keycode, t_fdata *fdata)
   if (keycode == ESC)
     exit(1);
   else if(keycode = LEFT)
-    fdata->x_delta -= 20 / fdata->zoom;
+    f->offset_x -= 20 / fdata->zoom;
   else if(keycode = RIGHT)
-    fdata->x_delta += 20 / fdata->zoom;
+    f->offset_x += 20 / fdata->zoom;
   else if(keycode = LEFT)
-    fdata->y_delta -= 20 / fdata->zoom;
+    f->offset_y -= 20 / fdata->zoom;
   else if(keycode = LEFT)
-    fdata->y_delta += 20 / fdata->zoom;
+    f->offset_y += 20 / fdata->zoom;
   draw_fractal(fdata, fdata->type);
 }
 
@@ -56,26 +98,26 @@ int	mouse_hook(int mouse_code, t_fdata *fdata)
   draw_fractal(fdata, fdata->type);
 }
 
-t_data  *ft_init_fdata(char **av)
+t_f  *ft_init_fdata(char **av)
 {
   t_fdata  fdata;
 
-  fdata = malloc(sizeof(fdata));
-  if (!fdata)
+  f = malloc(sizeof(t_f));
+  if (!f)
     exit(-1);
   if (!(av[1] && av[1][0] && !av[1][1] && (av[1][0] == 'j' || av [1][0] == 'm')))
-       exit(free(fdata), 1);
-  fdata.f_type = av[1][0];
-  fdata.x = 0;
-  fdata.y = 0;
-  f_data.custom_c = 0;
-  f_data.cx = 0.285;
-  f_data.cy = 0.01;
-  fdata.x_delta = 0;
-  fdata.y_delta = 0;
-  fdata.zoom = 0;
-  fdata.max_iter = 50;
-  fdata.color = 0xFCBE11;
+       exit(free(f), 1);
+  f.f_type = av[1][0];
+  f.x = 0;
+  f.y = 0;
+  f.custom_c = 0;
+  f.cx = 0.285;
+  f.cy = 0.01;
+  f.x_delta = 0;
+  f.y_delta = 0;
+  f.zoom = 0;
+  f.max_iter = 50;
+  f.color = 0xFCBE11;
   
   
 }
