921137335466b30d82a97edd42048bf47819b18b
Vilab <143715482+Vilab-unavailable@users.noreply.github.com>
2025-09-22 14:12:56 +0200

    Add files via upload

--- Cursus-42/pipex/pipex.h
+++ Cursus-42/pipex/pipex.h
@@ -6,14 +6,13 @@
 /*   By: vilabard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 15:49:27 by vilabard          #+#    #+#             */
-/*   Updated: 2025/08/07 15:49:30 by vilabard         ###   ########.fr       */
+/*   Updated: 2025/08/13 17:57:57 by vilabard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-//# include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdio.h>
@@ -21,9 +20,10 @@
 # include <fcntl.h>
 # include "libft/libft.h"
 
-
-
-
-
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+void	ft_freetable(char **tab);
+char	**ft_get_paths(char **envp);
+char	*ft_which_path(char *cmd, char **envp);
+void	ft_execute(char *str_cmd, char **envp);
 
 #endif
